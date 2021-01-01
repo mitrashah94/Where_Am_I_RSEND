9c5c0290bf95dfd1a57acd725b5effdf169d46d1
mitrashah94 <mitrashah94@gmail.com>
2020-12-31 19:41:59 -0500

    Simulation Setup

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mitra/Where_Am_I_RSEND/catkin_ws/src
+# Build directory: /home/mitra/Where_Am_I_RSEND/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("pgm_map_creator")
